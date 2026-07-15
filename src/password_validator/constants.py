39c83b50ca8de94c4f3d18bdcc9378ee158d3466
Shamimur-Shuvo <shamimur.shuvo@komute.ca>
2026-07-15 14:48:36 -0400

    Add basic initialization or installation of the package

--- src/password_validator/constants.py
+++ src/password_validator/constants.py
@@ -5,3 +5,46 @@ All constants used in the password_validator package are defined here.
 - DEFAULT_MIN_LENGTH
 - DEFAULT_ENTROPY
 """
+# Password defaults
+DEFAULT_MIN_LENGTH = 8
+DEFAULT_MAX_LENGTH = 64
+
+# Character defaults
+DEFAULT_SPECIAL_CHARACTERS = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~"
+
+DEFAULT_MIN_UPPERCASE = 1
+DEFAULT_MIN_LOWERCASE = 1
+DEFAULT_MIN_DIGITS = 1
+DEFAULT_MIN_SPECIAL = 1
+
+# Security defaults
+DEFAULT_MAX_REPEAT_COUNT = 3
+DEFAULT_MIN_ENTROPY = 40
+
+# Password policies
+DEFAULT_ALLOW_SPACES = False
+DEFAULT_CHECK_SEQUENTIAL = True
+DEFAULT_CHECK_COMMON_PASSWORDS = True
+DEFAULT_CHECK_DICTIONARY = True
+
+# Regex
+DEFAULT_CUSTOM_REGEX = None
+
+# Language defaults
+DEFAULT_LANGUAGE = "en"
+
+# Common sequences and patterns
+SEQUENTIAL_PATTERNS = [
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "0123456789"
+]
+
+# Common password file
+DEFAULT_COMMON_PASSWORDS_FILE = "resources/common_passwords.txt"
+DEFAULT_DICTIONARY_FILE = "resources/dictionary.txt"
+
+# Error messages
+DEFAULT_ERROR_PREFIX = (
+    "Password validation failed"
+)
--- src/password_validator/version.py
+++ src/password_validator/version.py
@@ -1,3 +1,7 @@
 """
 Version of the password_validator package.
 """
+__title__ = "password_validator"
+__version__ = "1.0.0"
+__author__ = "Md Shamimur Rahman Shuvo"
+__description__ = "A Python package for validating passwords based on various rules and criteria."
